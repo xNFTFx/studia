26207de81245217f842c17efd3ccd14df2384cc7
Szymon <szbuchowicz@gmail.com>
2024-03-18 16:23:59 +0100

    lol zajecia 3
    xd

--- pk_Szymon_Buchowicz/source/cwiczenia3.c
+++ pk_Szymon_Buchowicz/source/cwiczenia3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 
 int podzielna_przez_3_lub_5(int liczba){
@@ -56,17 +57,43 @@ int test_potega_2(){
     }
 }
 
-int montecarlo(dokladnosc){
+int montecarlo(int dokladnosc){
     srand((unsigned)time(0));
-    for( int i = 0, i < dokladnosc; i++){
-            float x = rand() % (1);
-            float y = rand() % (1);
-            printf("%f %f \n", x, y);
+    int promien = 1000000;
+    int w_kole = 0;
+    for( int i = 0; i < dokladnosc; i++){
+            double x = rand() % (promien);
+            double y = rand() % (promien);
+            if(sqrt(x*x + y*y) <= promien){w_kole+=1;}
     }
+    printf("%f", (double)w_kole*4/dokladnosc);
 }
 
+void zadanie_5(){
+    int n;
+    printf("podaj liczbe calkowita n\n");
+    if(scanf("%d", &n)<1 || n<0)
+    { printf("niepoprawne dane");}
+    float tab[n];
+    srand((unsigned)time(0));
+    if(n == 0){printf("Wybrales n = 0, wiec min, max, srednia i suma tez sa rowne 0 ;)\n");}
+    float suma = 0;
+    float min = 50;
+    float max = 0;
+    int licznik = 0;
+    for(int i= 0; i< n; i++){
+        tab[i] = (rand() % (100+1)) -50;
+        if(tab[i] > 0){       
+            suma += tab[i];
+            licznik+=1;
+            if(min > tab[i]){min = tab[i];}
+            if(max < tab[i]){max = tab[i];}
+        }
+    }
+    printf("min: %f max: %f sum: %f avg: %f", min, max, suma, suma/licznik);
+}
 
 int main()
 {
-montecarlo(10) ;
+montecarlo(500000000);
 }
--- pk_Szymon_Buchowicz/source/cwiczenia3.h
+++ pk_Szymon_Buchowicz/source/cwiczenia3.h
@@ -1,3 +1,8 @@
 #pragma once
 
+int podzielna_przez_3_lub_5();
 void suma_i_srednia_podzielnych_przez_3_lub_5();
+int potega_2();
+int test_potega_2();
+int montecarlo();
+void zadanie_5();
